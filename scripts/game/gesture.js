e03ac6a7d6e15ba6f6be559eebf1a67c0b07d631
Prasanna <prasanmouli@gmail.com>
2015-09-05 21:55:02 -0400

    fine-tuning gesture responses. redeable code.

--- scripts/game/gesture.js
+++ scripts/game/gesture.js
@@ -1,69 +1,62 @@
 var info = document.getElementById('main');
 
-var scale = 2/5, step = 9; /* game to world ratio */
+var scale = 2/5, step = 4; /* game to world ratio */
 var fingerAlignment = [], delta = [], pos = 0;
 var coordinates = {}, data = [], i = 0, j = 0, k = 0, start = 0, fingers;
 
 function evaluateGesture(fingerAlignment){
-  var fingerWeights = [], j = 0, move = [];
-  for(var j=0; j<3; j++){
+    var velocity = 1, friction = 0.5;
+    var fingerWeights = [], j = 0, move = [];
+    for(var j=0; j<3; j++){
     fingerWeights[j] = 0;
     for(var i=0; i<fingerAlignment.length; i++)
       fingerWeights[j] += fingerAlignment[i][j];
-    if(fingerWeights[j] >= 4)
-      move[j] = 1.5;
-    else if(fingerWeights[j] <= -4)
-      move[j] = -1.5;
+    if(fingerWeights[j] >= 4){
+      velocity *= friction;
+      move[j] = velocity;
+    }
+    else if(fingerWeights[j] <= -4){
+      velocity *= friction;
+      move[j] = -velocity;
+    }
     else if(fingerWeights[j] == 0)
       move[j] = 0
-  }
-  return move;
+    }
+    return move;
 }
 
 Leap.loop(function(frame){
-  frame.hands.forEach(function(hand, index) {
-    //console.log(hand.roll());
-  });
-
-  for(i = 0, fingers = frame.pointables.length; i < fingers; i++)
+    for(i = 0, fingers = frame.pointables.length; i < fingers; i++)
     data[i] = frame.pointables[i].tipPosition;
-  //console.log(fingers);
-  if(fingers != 0){
-    coordinates[j] = data;
-    //console.log(coordinates)
-    if(j%step == 0 && j>=step){
-      start = j-step;
-      for (k = 0; k < fingers; k++){
-        //console.log('Finger : '+k);
-        delta = [];
-        for (pos = 0; pos < 3; pos++){
-          if(coordinates[j-step][k] != undefined){
-            delta[pos] = coordinates[j][k][pos] - coordinates[j-step][k][pos];
-            if(Math.abs(delta[pos]) > (1/scale))
-              delta[pos] = delta[pos] > 0 ? 1 : -1;
-            else
-              delta[pos] = 0;
-          }
+    if(fingers != 0){
+        coordinates[j] = data;
+        if(j%step == 0 && j>=step){
+            start = j-step;
+            for (k = 0; k < fingers; k++){
+            delta = [];
+            for (pos = 0; pos < 3; pos++){
+                if(coordinates[j-step][k] != undefined){
+                delta[pos] = coordinates[j][k][pos] - coordinates[j-step][k][pos];
+                if(Math.abs(delta[pos]) > (1/scale))
+                  delta[pos] = delta[pos] > 0 ? 1 : -1;
+                else
+                  delta[pos] = 0;
+                }
+            }
+            fingerAlignment[k] = delta;
+            var move = evaluateGesture(fingerAlignment);
+            if(move[0]!==undefined) cube.Mesh.position.x += move[0];
+            if(move[1]!==undefined) cube.Mesh.position.y += move[1];
+            frame.hands.forEach(function(hand){
+                radians = hand.roll();
+                    if(radians > Math.PI/4 && radians < Math.PI/2) worldRotate(1);
+                    else if(radians > -Math.PI/2 && radians < -Math.PI/4) worldRotate(-1);
+                });
+            }
+            for (k = j-1; k >= start; k--)
+                delete coordinates[k];
         }
-        fingerAlignment[k] = delta;
-        var move = evaluateGesture(fingerAlignment);
-        //console.log(cube.position.x, move[0]);
-        if(move[0]!==undefined) cube.position.x += move[0];
-        if(move[1]!==undefined) cube.position.y += move[1];
-        frame.hands.forEach(function(hand){
-          radians = hand.roll();
-          console.log(radians*180/Math.PI);
-          if(radians > Math.PI/4 && radians < Math.PI/2) worldRotate(1);
-          else if(radians > -Math.PI/2 && radians < -Math.PI/4) worldRotate(-1);
-        });
-        
-      }
-      for (k = j-1; k >= start; k--)
-        delete coordinates[k];
-      //console.log(coordinates);
+        data = [];
+        j++;
     }
-    data = [];
-    j++;
-    //console.log(fingerAlignment[0],fingerAlignment[1],fingerAlignment[2],fingerAlignment[3],fingerAlignment[4]);
-  }
 });
--- scripts/game/main.js
+++ scripts/game/main.js
@@ -1,103 +1,70 @@
-var div = document.getElementById('main');
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 18, 1000);
-var world = new THREE.Object3D();
-var worldRot = false;
-
-var renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-div.appendChild(renderer.domElement);
-renderer.domElement.id = 'game';
-renderer.domElement.style.background = '#FFFFFF';
-
-var colorSets = [[0x00FF00,0x90C695,0x26A65B],[0xFF0000,0xD35400,0xF4B350],[0x0000FF,0x6C7A89,0x95A5A6]], colorCode;
-
-var obstacleMaterial = new THREE.MeshBasicMaterial( {color: 0xCCCCCC, side:THREE.DoubleSide} );
-var obstacleGeometry = new THREE.PlaneGeometry(window.innerWidth/32, window.innerHeight/32);
-var obstacle = new THREE.Mesh(obstacleGeometry, obstacleMaterial);
-obstacle.rotation.z = Math.PI/2;
-obstacle.position.z = -50;
-world.add(obstacle);
-  
-var render = function () {
-	requestAnimationFrame(render);
-	obstacle.position.z += 0.5;
-	renderer.render(scene, camera);
-};
-
-function init(){
+/* Globals */
+var colorSets = [
+	['#90C695','#90C695','#26A65B'],
+	['#E87E04','#D35400','#E87E04'],
+	['#6C7A89','#95A5A6', '#6C7A89']
+	],
 	colorCode = Math.floor(Math.random()*3);
 
-	var geometry = new THREE.BoxGeometry(10, 10, 10);
-	var material = new THREE.MeshBasicMaterial({color: colorSets[colorCode][0], wireframe: true, wireframeLinewidth: 2});
-	cube = new THREE.Mesh(geometry, material);
-	cube.rotation.x = 0;
-	scene.add(cube);
+var div = document.getElementById('main'),
+	scene = new THREE.Scene(),
+	camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 18, 1000),
+	world = new THREE.Object3D(),
+	worldRot = false;
 
-	camera.position.z = 50;
+var renderer = new THREE.WebGLRenderer({
+	antialias:true, 
+	alpha:true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.physicallyBasedShading = true;
+renderer.domElement.id = 'game';
+renderer.domElement.style.background = '#040404';
+div.appendChild(renderer.domElement);
 
-	var floorMaterial = new THREE.MeshBasicMaterial( {color:colorSets[colorCode][1], side:THREE.DoubleSide} );
-	var wallMaterial = new THREE.MeshBasicMaterial( {color:colorSets[colorCode][2], side:THREE.DoubleSide} );
-	var geometry = new THREE.PlaneGeometry(window.innerWidth/8, window.innerHeight/4);
 
-	// floor and ceilings
-	var floor = new THREE.Mesh(geometry, floorMaterial);
-	floor.position.y = -window.innerHeight/16;
-	floor.rotation.x = Math.PI/2;
-	world.add(floor);
-	
-	var ceiling = new THREE.Mesh(geometry, floorMaterial);
-	ceiling.position.y = window.innerHeight/16;
-	ceiling.rotation.x = Math.PI/2;
-	world.add(ceiling);
+/* Texture Generation */
+function generateTexture(color, option) {
+	var size = 512;
+	canvas = document.createElement( 'canvas' );
+	canvas.width = size;
+	canvas.height = size;
+	var context = canvas.getContext( '2d' );
+	context.rect( 0, 0, size, size );
+	var gradient;
+	if(option == 1){
+	    gradient = context.createLinearGradient(0,0,size/1.9,size/1.6);
+		gradient.addColorStop(0, color);	
+	    gradient.addColorStop(1, 'black'); 
+	}
+	else{
+	    gradient = context.createLinearGradient(0,0,500,500);
+		gradient.addColorStop(0, color);
+		gradient.addColorStop(1, 'black'); 
+	}
+	context.fillStyle = gradient;
+	context.fill();
 
-	// walls
-	var wall1 = new THREE.Mesh(geometry, wallMaterial);
-	wall1.position.x = -window.innerHeight/16;
-	wall1.rotation.y = Math.PI/2;
-	world.add(wall1);
-
-	var wall2 = new THREE.Mesh(geometry, wallMaterial);
-	wall2.position.x = window.innerHeight/16;
-	wall2.rotation.y = Math.PI/2;
-	world.add(wall2);
-
-	window.addEventListener('keyup', function(e){
-		if(e.keyCode == 37)
-			worldRotate(-1);
-		else if(e.keyCode == 39)
-			worldRotate(1);
-	});
-
-	/* // create a point light
-	var pointLight = new THREE.PointLight('white');
-
-	// set its position
-	pointLight.position.x = -1000;
-	pointLight.position.y = 0;
-	pointLight.position.z = 1000;
-	pointLight.intensity = 2.9;
-	pointLight.distance = 10000;
-
-	// add to the scene
-	scene.add(pointLight);*/
-	scene.add(world);
-
-	render();
+	return canvas;
 }
 
+/* Solids / Planes object */
+var solids = function(w, h, d, MeshBasic, type){
+	this.Geometry = (type == 'B') ? new THREE.BoxGeometry(w,h,d) : new THREE.PlaneGeometry(w,h);
+	this.Material = new THREE.MeshBasicMaterial(MeshBasic);
+}
+
+/* rotate the world about Z */
 worldRotate = function(sense){
 	if(worldRot == true)
 		return;
 	else 
 		worldRot = true;
-	angle = world.rotation.z, teta = 0;
+	var angle = world.rotation.z, teta = 0;
 	var T = setInterval(function(){
 		teta += sense*Math.PI/64;
 		angle += sense*Math.PI/64;
 		world.rotation.z = angle;
-		console.log(angle, world.rotation.z);
 		if((teta>=(Math.PI/2) && sense==1) || (teta<=-(Math.PI/2) && sense==-1)){
 			clearInterval(T);
 			worldRot = false;
@@ -105,4 +72,111 @@ worldRotate = function(sense){
 	}, 1000/60, false);
 }
 
+/* The LOOP; reason for all awesomeness and misery. */
+render = function (AA){
+	requestAnimFrame(function(){render(AA);});
+	AA.position.z += 2;
+	renderer.render(scene, camera);
+};
+
+/* It all begins here */
+init = function(){
+
+	/* Cube; Hero; You; Whatever! */
+	var cubeTexture = new THREE.Texture(generateTexture(colorSets[colorCode][0],2));
+	cubeTexture.needsUpdate = true;
+	var cubeParams = {
+		map: cubeTexture, 
+		color: colorSets[colorCode][0]
+		//wireframe: true, 
+		//wireframeLinewidth: 2
+	},
+	cubeWidth = 10,
+	cubeHeight = 10,
+	cubeDepth = 10;
+
+	cube = new solids(cubeWidth, cubeHeight, cubeDepth, cubeParams, 'B');
+	cube.Mesh = new THREE.Mesh(cube.Geometry, cube.Material);
+	cube.Mesh.position.z = 0;
+	cube.Mesh.rotation.x = 0;
+	scene.add(cube.Mesh);
+
+	camera.position.z = 50;
+
+	/* The bad guys. Not very smart (not their fault) */	
+	var obstacleTexture = new THREE.Texture(generateTexture(colorSets[colorCode][0],2));
+	obstacleTexture.needsUpdate = true;
+	var obstacleParams = {
+		map: obstacleTexture, 
+		color: colorSets[colorCode][0]
+		//wireframe: true, 
+		//wireframeLinewidth: 2
+	};
+
+	var obstacle = new solids(50, 30, 2, obstacleParams, 'B');
+	obstacle.Mesh = new THREE.Mesh(obstacle.Geometry, obstacle.Material);
+	obstacle.Mesh.position.y = -20;
+	obstacle.Mesh.rotation.z = 0;
+	obstacle.Mesh.position.z = -250;
+	obstacle.Mesh.rotation.x = Math.PI/8;
+	world.add(obstacle.Mesh);
+
+
+	/* The Surrounding; Our Viewport; Floors, Ceilings and Walls */
+	var floorTexture = new THREE.Texture(generateTexture(colorSets[colorCode][1],1));
+	floorTexture.needsUpdate = true;
+	var floorParams = {
+		map : floorTexture, 
+		color : colorSets[colorCode][1], 
+		side : THREE.DoubleSide
+	}, 
+	floorWidth = window.innerWidth/3, 
+	floorHeight = window.innerHeight/1.5,
+	floorDepth = 0;
+	
+	var floor = ceiling = new solids(floorWidth, floorHeight, floorDepth, floorParams, 'P');
+	floor.Mesh = new THREE.Mesh(floor.Geometry, floor.Material);
+	floor.Mesh.position.y = window.innerHeight/16;
+	floor.Mesh.rotation.x = Math.PI/2;
+	world.add(floor.Mesh);
+	ceiling.Mesh = new THREE.Mesh(ceiling.Geometry, ceiling.Material);
+	ceiling.Mesh.position.y = -window.innerHeight/16;
+	ceiling.Mesh.rotation.x = Math.PI/2;
+	world.add(ceiling.Mesh);	
+
+	var wallTexture = new THREE.Texture(generateTexture(colorSets[colorCode][2],1));
+	wallTexture.needsUpdate = true;
+	var wallParams = {
+		map : wallTexture, 
+		color : colorSets[colorCode][2], 
+		side : THREE.DoubleSide
+	}, 
+	wallWidth = window.innerWidth/3, 
+	wallHeight = window.innerHeight/1.5,
+	wallDepth = 0,
+	wallTexture = generateTexture(colorSets[colorCode][2],1);
+
+	var leftW = rightW = new solids(wallWidth, wallHeight, wallDepth, wallParams, 'P');
+	leftW.Mesh = new THREE.Mesh(leftW.Geometry, leftW.Material);
+	leftW.Mesh.position.x = window.innerHeight/16;
+	leftW.Mesh.rotation.y = Math.PI/2;
+	world.add(leftW.Mesh);
+	rightW.Mesh = new THREE.Mesh(rightW.Geometry, rightW.Material);
+	rightW.Mesh.position.x = -window.innerHeight/16;
+	rightW.Mesh.rotation.y = Math.PI/2;
+	world.add(rightW.Mesh);
+
+	/* Dump everything to the scene, render and pray! */
+	scene.add(world);
+	render(obstacle.Mesh);
+}
+
+window.addEventListener('keyup', function(e){
+	if(e.keyCode == 37)
+		worldRotate(-1);
+	else if(e.keyCode == 39)
+		worldRotate(1);
+});
+
+/* So it begins */
 init();
